1cfecdc1872b59103a070301816fdfabc728db92
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-06-13 08:41:11 +0300

    Finished lecture #152, refactored user model to work with a build function.

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -7,6 +7,12 @@ interface UserAttributes {
   password: string;
 }
 
+// An inteface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<any> {
+  build(attributes: UserAttributes): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -18,10 +24,9 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attributes: UserAttributes) => {
+userSchema.statics.build = (attributes: UserAttributes) => {
   return new User(attributes);
 };
+const User = mongoose.model<any, UserModel>('User', userSchema);
 
-export { User, buildUser };
+export { User };
