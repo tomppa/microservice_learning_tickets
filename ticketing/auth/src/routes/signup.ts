79fb6b9ce1ab30469aa965717cf3e5ff8a79c1b0
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-06-13 08:55:34 +0300

    Finished lecture #155, added functionality to create new user in DB.

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+
+import { User } from './../models/user';
 import { RequestValidationError } from './../errors/request-validation-error';
-import { DatabaseConnectionError } from './../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -23,12 +24,17 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      console.log('Email in use');
+      return res.send();
+    }
 
-    // new User({ email, password })
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
