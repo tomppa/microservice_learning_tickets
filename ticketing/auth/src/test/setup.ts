41df87c85e7b80a33aaee6842eaaf167cf89f00b
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-06-19 02:19:51 +0300

    Finished lecture #194, wrote the first test to verify signing up works.

--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -5,6 +5,8 @@ import { app } from '../app';
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = 'my-secret';
+
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
