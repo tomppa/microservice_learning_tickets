11e254e2918b85a742b5608b1addd3f1019ace6b
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-06-19 22:05:51 +0300

    Finished lecture #204, worked a bit more on current user tests.

--- ticketing/auth/src/routes/__test__/current-user.test.ts
+++ ticketing/auth/src/routes/__test__/current-user.test.ts
@@ -4,3 +4,9 @@ it('should respond with details about current user', async () => {
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
+
+it('should respond with null, if not authenticated', async () => {
+  const response = await global.currentUser([''], 200);
+
+  expect(response.body.currentUser).toBeNull();
+});
