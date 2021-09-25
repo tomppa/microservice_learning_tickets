d9b1c0a9b4c30352aa0073de7d1929a2cd456177
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-09-25 21:48:23 +0300

    Finished lecture #231, modified index.js on client to get the current user details.

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,16 +1,33 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
+  // This is executed from the browser.
   console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
 
   return <h1>Landing page</h1>;
 };
 
-LandingPage.getInitialProps = () => {
-  return { color: 'red' };
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'demo-ticketing.com',
+        },
+      }
+    );
+
+    return {};
+  } else {
+    // we are on the browser!
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+  return {};
 };
 
 export default LandingPage;
