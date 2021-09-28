e3cb3635530a141a764c613584bdc5e35a5b66b2
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-09-28 23:30:13 +0300

    Finished lecture #261, start building tests before implementing anything.

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('should have a route handler listening to /api/tickets for post requests', async () => {
+  // foobar
+});
+
+it('should only be accessible, if the user is signed in', async () => {
+  // foobar
+});
+
+it('should return an error, if an invalid title is provided', async () => {
+  // foobar
+});
+
+it('should return an error, if an invalid price is provided', async () => {
+  // foobar
+});
+
+it('should create a ticket with valid inputs', async () => {
+  // foobar
+});
