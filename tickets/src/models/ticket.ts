56f404991beb77b1fcdec736d95d65986706d638
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-10-17 00:28:36 +0300

    Finished lecture #269, implemented remainder of the ticket model.

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -21,3 +21,39 @@ interface TicketDocument extends mongoose.Document {
   price: string;
   userId: string;
 }
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attributes: TicketAttributes) => {
+  return new Ticket(attributes);
+};
+
+const Ticket = mongoose.model<TicketDocument, TicketModel>(
+  'Ticket',
+  ticketSchema
+);
+
+export { Ticket };
